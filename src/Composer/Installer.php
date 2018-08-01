a781bb4866a6779e5be026d179bf0450d41ac75d
张学会 <zhangxuehui@shopex.cn>
2018-08-01 18:54:29 +0800

    init

---
+++ src/Composer/Installer.php
@@ -0,0 +1,52 @@
+<?php
+namespace Laravelos\ModuleInstaller;
+
+use Composer\Package\PackageInterface;
+use Composer\Installer\LibraryInstaller;
+use Composer\Installers\BaseInstaller;
+
+class Installer extends BaseInstaller
+{
+
+    protected $locations = array(
+        'library' => 'libraries/{$name}/',
+    );
+        /**
+     * Return the install path based on package type.
+     *
+     * @param  PackageInterface $package
+     * @param  string           $frameworkType
+     * @return string
+     */
+    public function getInstallPath(PackageInterface $package, $frameworkType = '')
+    {
+        $type = $this->package->getType();
+
+        $prettyName = $this->package->getPrettyName();
+        if (strpos($prettyName, '/') !== false) {
+            list($vendor, $name) = explode('/', $prettyName);
+        } else {
+            $vendor = '';
+            $name = $prettyName;
+        }
+
+        $availableVars = $this->inflectPackageVars(compact('name', 'vendor', 'type'));
+
+        $extra = $package->getExtra();
+        if (!empty($extra['installer-name'])) {
+            $moduleName = $extra['installer-name'];
+        }else{
+            $moduleName = ucfirst($vendor).ucfirst($name);
+        }
+        $moduleName = ucfirst($moduleName);
+        return "Modules/".$moduleName;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function supports($packageType)
+    {
+        return 'laravelos-module' === $packageType;
+    }
+}
---
+++ src/Composer/Plugin.php
@@ -0,0 +1,15 @@
+<?php
+namespace Laravelos\ModuleInstaller;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+class Plugin implements PluginInterface
+{
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        $installer = new Installer($io, $composer);
+        $composer->getInstallationManager()->addInstaller($installer);
+    }
+}
